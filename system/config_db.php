d7c34948ab9ab985a0eb91cdf701c51985bf386d
Mark Tomicki <mark@lolquest.net>
2015-04-07 15:25:09 +0200

    ignore

--- system/config_db.php
+++ system/config_db.php
@@ -2,7 +2,7 @@
 
 // MySQL-Config
 define("MYSQL_TYPE", "mysqli");
-define("MYSQL_USER", "root");
-define("MYSQL_PW", "");
+define("MYSQL_USER", "crawler");
+define("MYSQL_PW", "7ZGK97YA8Q2Jm2KU");
 define("MYSQL_DB", "nurf");
 define("MYSQL_HOST", "localhost");
