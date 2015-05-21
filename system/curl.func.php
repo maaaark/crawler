4aae46ea5d9d9380046aa0e0984aa7e0429f899b
Benedict Romp <b.romp@meeva.de>
2015-05-21 15:30:45 +0200

    Bugfix

--- system/curl.func.php
+++ system/curl.func.php
@@ -12,7 +12,7 @@ function curl_file($url, $use_useragent = false){
    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,5); 
-	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 5);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	
 	if($use_useragent){
