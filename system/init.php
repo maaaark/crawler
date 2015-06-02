620d8d475271c579bd9e848fbdbccb0a4784e959
Benedict Romp <benedict.romp@googlemail.com>
2015-06-02 20:46:02 +0200

    Bugfix

--- system/init.php
+++ system/init.php
@@ -14,6 +14,7 @@ require_once dirname(__FILE__).'/config_db.php';
 require_once dirname(__FILE__).'/mysql.class.php';
 $GLOBALS["db"] = new MySQL(MYSQL_TYPE, MYSQL_HOST, MYSQL_USER, MYSQL_PW, MYSQL_DB);
 $GLOBALS["db_fi"] = new MySQL(MYSQL_FI_TYPE, MYSQL_FI_HOST, MYSQL_FI_USER, MYSQL_FI_PW, MYSQL_FI_DB);
+$GLOBALS["db_nw"] = new MySQL(MYSQL_NW_TYPE, MYSQL_NW_HOST, MYSQL_NW_USER, MYSQL_NW_PW, MYSQL_NW_DB);
 
 session_start();
 
